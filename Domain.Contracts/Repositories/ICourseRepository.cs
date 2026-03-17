72a06f558ffeee5bed20cea75eb74454ce62fb36
Jacob <jackan202@gmail.com>
2026-03-17 16:42:43 +0100

    Course Repository

---
+++ Domain.Contracts/Repositories/ICourseRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models.Entities;
+
+namespace Domain.Contracts.Repositories;
+
+public interface ICourseRepository : IRepositoryBase<Course>
+{
+    Task<IReadOnlyCollection<Course>> GetAllCourses();
+    Task<Course?> GetCourseFromUserId(Guid UserId);
+}
---
+++ LMS.Infractructure/Repositories/CourseRepository.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Contracts.Repositories;
+using Domain.Models.Entities;
+using LMS.Infractructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Infractructure.Repositories;
+
+public class CourseRepository : RepositoryBase<Course>, ICourseRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public CourseRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<IReadOnlyCollection<Course>> GetAllCourses()
+    {
+        return await _context.Courses
+                        .AsNoTracking()
+                        .ToListAsync();
+    }
+
+    public async Task<Course?> GetCourseFromUserId(Guid UserId)
+    {
+        string userIdStr = UserId.ToString();
+        return await _context.Courses
+                        .AsNoTracking()
+                        .Where(c => c.Students.FirstOrDefault(u => u.Id == userIdStr) != null)
+                        .FirstOrDefaultAsync();
+    }
+}
