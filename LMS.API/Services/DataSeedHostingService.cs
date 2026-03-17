deedc20aa49553092500e3db0cd131830805b554
Jacob <jackan202@gmail.com>
2026-03-17 14:08:07 +0100

    add seeding

--- LMS.API/Services/DataSeedHostingService.cs
+++ LMS.API/Services/DataSeedHostingService.cs
@@ -2,6 +2,7 @@
 using LMS.Infractructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using NuGet.Packaging;
 
 namespace LMS.API.Services;
 
@@ -39,32 +40,65 @@ public class DataSeedHostingService : IHostedService
         if (!env.IsDevelopment()) return;
 
         var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        if (await context.Users.AnyAsync(cancellationToken)) return;
+        IReadOnlyCollection<ApplicationUser> users = [];
+        if (!(await context.Users.AnyAsync(cancellationToken))) {
+            userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
-        userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-        roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            ArgumentNullException.ThrowIfNull(roleManager, nameof(roleManager));
+            ArgumentNullException.ThrowIfNull(userManager, nameof(userManager));
 
-        ArgumentNullException.ThrowIfNull(roleManager, nameof(roleManager));
-        ArgumentNullException.ThrowIfNull(userManager, nameof(userManager));
-
-        try
-        {
-            await AddRolesAsync([TeacherRole, StudentRole]);
-            await AddDemoUsersAsync();
-            await AddUsersAsync(20);
-            logger.LogInformation("Seed complete");
+            try {
+                await AddRolesAsync([TeacherRole, StudentRole]);
+                await AddDemoUsersAsync();
+                users = await AddUsersAsync(20);
+                logger.LogInformation("Seed complete");
+            } catch (Exception ex) {
+                logger.LogError($"Data seed fail with error: {ex.Message}");
+                throw;
+            }
         }
-        catch (Exception ex)
-        {
-            logger.LogError($"Data seed fail with error: {ex.Message}");
-            throw;
+
+        IReadOnlyCollection<Course> courses = [];
+        IReadOnlyCollection<Module> modules = [];
+        IReadOnlyCollection<ActivityType> activityTypes = [];
+        IReadOnlyCollection<Activity> activities = [];
+        IReadOnlyCollection<DocumentType> documentTypes = [];
+        IReadOnlyCollection<Document> documents = [];
+        if (!(await context.Courses.AnyAsync(cancellationToken))) {
+            courses = await AddCoursesToDb(context);
+            var course = courses.First();
+            course.Students.Add((await userManager.FindByEmailAsync("student@test.com"))!);
+            course.Students.Add((await userManager.FindByEmailAsync("teacher@test.com"))!);
+            course.Students.AddRange(users);
         }
+
+        if (!(await context.Modules.AnyAsync(cancellationToken))) {
+            modules = await AddModulesToCourse(courses.First(), context);
+        }
+
+        if (!(await context.ActivityTypes.AnyAsync(cancellationToken))) {
+            activityTypes = await AddActivityTypesToDb(context);
+        }
+
+        if (!(await context.Activities.AnyAsync(cancellationToken))) {
+            activities = await AddActivitiesToModule(modules.First(), activityTypes.First(), context);
+        }
+
+        if (!(await context.DocumentTypes.AnyAsync(cancellationToken))) {
+            // TODO: Add document types
+        }
+
+        if (!(await context.Documents.AnyAsync(cancellationToken))) {
+            // TODO: Add documents
+        }
+
+        await context.SaveChangesAsync(cancellationToken);
     }
 
     private async Task AddRolesAsync(string[] rolenames)
     {
-        foreach (string rolename in rolenames)
-        {
+        foreach (string rolename in rolenames) {
             if (await roleManager.RoleExistsAsync(rolename)) continue;
             var role = new IdentityRole { Name = rolename };
             var res = await roleManager.CreateAsync(role);
@@ -74,16 +108,20 @@ public class DataSeedHostingService : IHostedService
     }
     private async Task AddDemoUsersAsync()
     {
-        var teacher = new ApplicationUser
-        {
+        var teacher = new ApplicationUser {
             UserName = "teacher@test.com",
-            Email = "teacher@test.com"
+            Email = "teacher@test.com",
+            EmailConfirmed = true,
+            FirstName = "Role",
+            LastName = "Teacher"
         };
-        
-        var student = new ApplicationUser
-        {
+
+        var student = new ApplicationUser {
             UserName = "student@test.com",
-            Email = "student@test.com"
+            Email = "student@test.com",
+            EmailConfirmed = true,
+            FirstName = "Role",
+            LastName = "Student"
         };
 
         await AddUserToDb([teacher, student]);
@@ -95,15 +133,21 @@ public class DataSeedHostingService : IHostedService
         if (!studentRoleResult.Succeeded) throw new Exception(string.Join("\n", studentRoleResult.Errors));
     }
 
-    private async Task AddUsersAsync(int nrOfUsers)
+    private async Task<IReadOnlyCollection<ApplicationUser>> AddUsersAsync(int nrOfUsers)
     {
         var faker = new Faker<ApplicationUser>("sv").Rules((f, e) =>
         {
+            e.FirstName = f.Person.FirstName;
+            e.LastName = f.Person.LastName;
             e.Email = f.Person.Email;
             e.UserName = f.Person.Email;
+            e.EmailConfirmed = true;
         });
 
-        await AddUserToDb(faker.Generate(nrOfUsers));
+        IReadOnlyCollection<ApplicationUser> users = faker.Generate(nrOfUsers);
+
+        await AddUserToDb(users);
+        return users;
     }
 
     private async Task AddUserToDb(IEnumerable<ApplicationUser> users)
@@ -111,12 +155,89 @@ public class DataSeedHostingService : IHostedService
         var passWord = configuration["password"];
         ArgumentNullException.ThrowIfNull(passWord, nameof(passWord));
 
-        foreach (var user in users)
-        {
+        foreach (var user in users) {
             var result = await userManager.CreateAsync(user, passWord);
             if (!result.Succeeded) throw new Exception(string.Join("\n", result.Errors));
         }
     }
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
 
+    private async Task<IReadOnlyCollection<Course>> AddCoursesToDb(ApplicationDbContext context)
+    {
+        IReadOnlyCollection<Course> courses = [
+            new() {
+                Name = "Javascript",
+                Description = "Learn JS",
+                StartDate = new DateTime(2025, 10, 16, 8, 0, 0),
+                EndDate = new DateTime(2026, 4, 30, 16, 45, 0),
+            },
+            new() {
+                Name = ".NET",
+                Description = "Learn C# .NET",
+                StartDate = new DateTime(2026, 10, 16, 8, 0, 0),
+                EndDate = new DateTime(2027, 2, 10, 17, 0, 0)
+            }
+        ];
+
+        context.Courses.AddRange(courses);
+        return courses;
+    }
+
+    private async Task<IReadOnlyCollection<Module>> AddModulesToCourse(Course course, ApplicationDbContext context)
+    {
+        IReadOnlyCollection<Module> modules = [
+            new() {
+                Name = "Web API",
+                StartDate = new DateTime(2027, 1, 10, 8, 0, 0),
+                EndDate = new DateTime(2027, 2, 28, 17, 0, 0),
+                Course = course,
+            },
+            new() {
+                Name = "Frontend",
+                StartDate= new DateTime(2027, 3, 1, 8, 0, 0),
+                EndDate = new DateTime(2027, 4, 30, 17, 0, 0),
+                Course = course
+            }
+        ];
+
+        context.Modules.AddRange(modules);
+        return modules;
+    }
+
+    private async Task<IReadOnlyCollection<ActivityType>> AddActivityTypesToDb(ApplicationDbContext context)
+    {
+        IReadOnlyCollection<ActivityType> types = [
+            new() { Name = "Assignment" },
+            new() { Name = "Lecture" },
+            new() { Name = "E-Learning" },
+            new() { Name = "Examination" }
+        ];
+
+        context.ActivityTypes.AddRange(types);
+
+        return types;
+    }
+
+    private async Task<IReadOnlyCollection<Activity>> AddActivitiesToModule(Module module, ActivityType type, ApplicationDbContext context)
+    {
+        IReadOnlyCollection<Activity> activities = [
+            new() {
+                Name = "Lecture 1",
+                Type = type,
+                StartDate = new DateTime(2025, 10, 17, 9, 0, 0),
+                EndDate = new DateTime(2025, 10, 17, 12, 0, 0),
+                Module = module
+            },
+            new() {
+                Name = "Assignment 1",
+                Type = type,
+                StartDate= new DateTime(2025, 10, 17, 12, 0, 0),
+                EndDate = new DateTime(2025, 10, 31, 17, 0, 0),
+                Module = module
+            }
+        ];
+
+        context.Activities.AddRange(activities);
+        return activities;
+    }
 }
---
+++ LMS.Infractructure/Migrations/20260317124811_AddLmsTables.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS.Infractructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLmsTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CourseId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ActivityTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DocumentTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DocumentTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Modules",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CourseId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Modules_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Activities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TypeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ModuleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Activities_ActivityTypes_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "ActivityTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Activities_Modules_ModuleId",
+                        column: x => x.ModuleId,
+                        principalTable: "Modules",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Documents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TypeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UploadedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UploaderId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CourseId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ModuleId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ActivityId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Documents_Activities_ActivityId",
+                        column: x => x.ActivityId,
+                        principalTable: "Activities",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Documents_AspNetUsers_UploaderId",
+                        column: x => x.UploaderId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Documents_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Documents_DocumentTypes_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "DocumentTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Documents_Modules_ModuleId",
+                        column: x => x.ModuleId,
+                        principalTable: "Modules",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_CourseId",
+                table: "AspNetUsers",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_ModuleId",
+                table: "Activities",
+                column: "ModuleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_TypeId",
+                table: "Activities",
+                column: "TypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_ActivityId",
+                table: "Documents",
+                column: "ActivityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_CourseId",
+                table: "Documents",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_ModuleId",
+                table: "Documents",
+                column: "ModuleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_TypeId",
+                table: "Documents",
+                column: "TypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documents_UploaderId",
+                table: "Documents",
+                column: "UploaderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modules_CourseId",
+                table: "Modules",
+                column: "CourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Courses_CourseId",
+                table: "AspNetUsers",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Courses_CourseId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Documents");
+
+            migrationBuilder.DropTable(
+                name: "Activities");
+
+            migrationBuilder.DropTable(
+                name: "DocumentTypes");
+
+            migrationBuilder.DropTable(
+                name: "ActivityTypes");
+
+            migrationBuilder.DropTable(
+                name: "Modules");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_CourseId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CourseId",
+                table: "AspNetUsers");
+        }
+    }
+}
