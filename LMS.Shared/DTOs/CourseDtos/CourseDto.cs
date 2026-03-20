3f1cb9fc70d9e9a35c198c22fa7a2626bf0bd5c5
Jacob <jackan202@gmail.com>
2026-03-20 10:16:21 +0100

    add properties for date strings

--- LMS.Shared/DTOs/CourseDtos/CourseDto.cs
+++ LMS.Shared/DTOs/CourseDtos/CourseDto.cs
@@ -5,7 +5,9 @@ public sealed record CourseDto
     public string Name {get; set; } = string.Empty;
     public string Description {get; set; } = string.Empty;
     public DateOnly StartDate {get; set; }
+    public string StartDateStr => StartDate.ToString("yyyy-MM-dd");
     public DateOnly EndDate {get; set; }
+    public string EndDateStr => EndDate.ToString("yyyy-MM-dd");
     public int NumberOfStudents {get; set; }
     public ICollection<CourseModuleDto> Modules { get; set; } = [];
 }
