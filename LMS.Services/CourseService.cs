13b29c296aadb0e36b4ddb10e4191ab8f4b85e36
Jacob <jackan202@gmail.com>
2026-03-23 12:11:08 +0100

    update service

--- LMS.Services/CourseService.cs
+++ LMS.Services/CourseService.cs
@@ -2,6 +2,7 @@
 using Domain.Contracts.Repositories;
 using Domain.Models.Entities;
 using LMS.Shared.DTOs.CourseDtos;
+using Microsoft.Extensions.Logging;
 using Service.Contracts;
 
 namespace LMS.Services;
@@ -9,26 +10,34 @@ namespace LMS.Services;
 public class CourseService : ICourseService
 {
     private readonly IUnitOfWork _uow;
-    private readonly ICourseRepository _courseRepository;
     private readonly IMapper _mapper;
+    private readonly ILogger<CourseService> _logger;
 
-    public CourseService(IUnitOfWork uow, ICourseRepository courseRepository, IMapper mapper)
+    public CourseService(IUnitOfWork uow, IMapper mapper, ILogger<CourseService> logger)
     {
         _uow = uow;
-        _courseRepository = courseRepository;
         _mapper = mapper;
+        _logger = logger;
+    }
+
+    public async Task<bool> CreateCourse(CreateCourseDto createCourseDto, CancellationToken token)
+    {
+        Course course = _mapper.Map<Course>(createCourseDto);
+        _uow.CourseRepository.Create(course);
+        await _uow.CompleteAsync(token);
+        return true;
     }
 
     public async Task<IReadOnlyCollection<CourseDto>> GetAllCourses(AllCoursesParams param, CancellationToken token)
     {
-        IReadOnlyCollection<Course> courses = await _courseRepository.GetAllCourses(param, token);
+        IReadOnlyCollection<Course> courses = await _uow.CourseRepository.GetAllCourses(param, token);
         var courseDtos = _mapper.Map<IReadOnlyCollection<CourseDto>>(courses);
         return courseDtos ?? [];
     }
 
     public async Task<CourseDto?> GetCourseById(Guid id, string? currentStudentId, CancellationToken token)
     {
-        var course = await _courseRepository.GetCourseById(id, token);
+        var course = await _uow.CourseRepository.GetCourseById(id, token);
         if (course is null) return null;
         
         var userIds = course.Students.Select(u => u.Id);
@@ -42,7 +51,7 @@ public class CourseService : ICourseService
 
     public async Task<CourseDto?> GetCourseByUserId(Guid id, CancellationToken token)
     {
-        var course = await _courseRepository.GetCourseFromUserId(id, token);
+        var course = await _uow.CourseRepository.GetCourseFromUserId(id, token);
         if (course is null) return null;
         var courseDto = _mapper.Map<CourseDto>(course);
         return courseDto;
