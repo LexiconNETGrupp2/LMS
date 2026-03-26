6035b88e9b90fed20b6d81687d3024c380473ac0
Jacob <jackan202@gmail.com>
2026-03-26 08:41:16 +0100

    added a speical model to use in the form

---
+++ LMS.Blazor/LMS.Blazor.Client/Components/CreateCourse/CourseFormModel.cs
@@ -0,0 +1,13 @@
+﻿using LMS.Blazor.Client.Components.Modules;
+
+namespace LMS.Blazor.Client.Components.CreateCourse;
+
+public class CourseFormModel
+{
+    public Guid? Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateOnly? StartDate { get; set; }
+    public DateOnly? EndDate { get; set; }
+    public List<ModuleFormModel> Modules { get; set; } = [];
+}
