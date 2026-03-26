0efd1b29e6c8204fd7b322449245cd011e9975b9
Jacob <jackan202@gmail.com>
2026-03-26 08:43:58 +0100

    copy validation attributes from Dana

--- LMS.Blazor/LMS.Blazor.Client/Components/CreateCourse/CourseFormModel.cs
+++ LMS.Blazor/LMS.Blazor.Client/Components/CreateCourse/CourseFormModel.cs
@@ -1,13 +1,25 @@
 ﻿using LMS.Blazor.Client.Components.Modules;
+using System.ComponentModel.DataAnnotations;
 
 namespace LMS.Blazor.Client.Components.CreateCourse;
 
 public class CourseFormModel
 {
     public Guid? Id { get; set; }
+
+    [Required(ErrorMessage = "Kursnamnet är obligatoriskt.")]
+    [StringLength(120, ErrorMessage = "Kursnamnet får vara högst 120 tecken.")]
     public string Name { get; set; } = string.Empty;
+
+    [StringLength(500, ErrorMessage = "Beskrivningen får vara högst 500 tecken.")]
     public string Description { get; set; } = string.Empty;
+    
+    [Required(ErrorMessage = "Startdatum är obligatoriskt.")]
     public DateOnly? StartDate { get; set; }
+    
+    [Required(ErrorMessage = "Slutdatum är obligatoriskt.")]
     public DateOnly? EndDate { get; set; }
+    
+
     public List<ModuleFormModel> Modules { get; set; } = [];
 }
