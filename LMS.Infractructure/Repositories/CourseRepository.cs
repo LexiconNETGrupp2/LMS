ea01e0d78c28bf9337f64b9b07f06ea07757b473
Jacob <jackan202@gmail.com>
2026-03-30 14:51:48 +0200

    make repository use base methods. Excludes getting users since they're gonna be refactored elsewhere

--- LMS.Infractructure/Repositories/CourseRepository.cs
+++ LMS.Infractructure/Repositories/CourseRepository.cs
@@ -20,9 +20,7 @@ public class CourseRepository : RepositoryBase<Course>, ICourseRepository
 
     public async Task<IReadOnlyCollection<Course>> GetAllCourses(AllCoursesParams param, CancellationToken token)
     {
-        var query = _context.Courses
-                        .AsNoTracking()
-                        .AsQueryable();
+        var query = FindAll(trackChanges: false);
 
         if (param.AfterDate is not null) {
             query = query.Where(c => c.StartDate <= param.AfterDate);
@@ -44,8 +42,7 @@ public class CourseRepository : RepositoryBase<Course>, ICourseRepository
 
     public async Task<Course?> GetCourseById(Guid id, CancellationToken token)
     {
-        return await _context.Courses
-                        .AsNoTracking()
+        return await FindAll(trackChanges: false)
                         .Include(c => c.Modules)
                             .ThenInclude(m => m.Activities)
                         .Include(c => c.Students)
@@ -54,7 +51,7 @@ public class CourseRepository : RepositoryBase<Course>, ICourseRepository
 
     public async Task<Course?> GetCourseByIdTracked(Guid id, CancellationToken token)
     {
-        return await _context.Courses
+        return await FindAll(trackChanges: false)
                         .Include(c => c.Modules)
                         .Include(c => c.Students)
                         .FirstOrDefaultAsync(c => c.Id == id, token);
@@ -64,7 +61,7 @@ public class CourseRepository : RepositoryBase<Course>, ICourseRepository
     {
         var userIdStr = userId.ToString();
 
-        return await _context.Courses
+        return await FindAll(trackChanges: false)
                         .AsNoTracking()
                         .Where(c => c.Students.FirstOrDefault(u => u.Id == userIdStr) != null)
                         .Include(c => c.Modules)
